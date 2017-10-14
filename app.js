e67182740608cf39990392cf194783eb1f8063f9
Jonathan Arouca <jonathanarouca@gmail.com>
2017-10-14 15:25:24 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var players = [
+  { name: "Elise", playing: true },
+  { name: "MLG", playing: true },
+  { name: "D-Rod", playing: false },
+  { name: "DT", playing: true },
+  { name: "Kevin", playing: false }
+];
+
+var playersPlaying = _.filter(players, function(player) { return player.playing });
+
+duckduckGoose.play(playersPlaying);
